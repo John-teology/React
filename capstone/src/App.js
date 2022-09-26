41ec843be153a1629f75b8303b86eb9ebedcf7ef
John-teology <teologowilhlem@gmail.com>
2022-09-26 22:38:22 +0800

    update 09-26-22

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Die from "./components/Die";
+import Confetti from "react-confetti";
 
 function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+
+  function allNewDice() {
+    const arrayDice = [];
+    for (let i = 0; i < 10; i++) {
+      arrayDice.push({
+        id: i,
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+      });
+    }
+    return arrayDice;
+  }
+
+  function hold(id) {
+    setDice((prevDice) =>
+      prevDice.map((item) => ({
+        ...item,
+        isHeld: item.id === id ? !item.isHeld : item.isHeld,
+      }))
+    );
+  }
+
+  function rollDice() {
+    allEqual("value") && allEqual("isHeld")
+      ? setDice(allNewDice())
+      : setDice((prevDice) =>
+          prevDice.map((item) => ({
+            ...item,
+            value: !item.isHeld ? Math.ceil(Math.random() * 6) : item.value,
+          }))
+        );
+  }
+
+  function allEqual(keys) {
+    const vals = dice.map((item) => item[keys]);
+    return vals.every((item) => item === vals[0]);
+  }
+
+  const setOfDice = dice.map((item) => (
+    <Die
+      key={item.id}
+      id={item.id}
+      value={item.value}
+      isHeld={item.isHeld}
+      hold={hold}
+    />
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {allEqual("value") && allEqual("isHeld") && <Confetti />}
+      <h1>Tenzies</h1>
+      <h4>
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls
+      </h4>
+      <div className="dice-container">{setOfDice}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        {allEqual("value") && allEqual("isHeld") ? "Reset" : "Roll"}
+      </button>
+    </main>
   );
 }
 
